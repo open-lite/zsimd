13d1e9468e100c6853e14feff8665f371e73740e
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2024-06-04 17:24:48 -0600

    Add single-header file

---
+++ include/zsimd.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "zsimd/attrib/current_arch.h"
+#include "zsimd/attrib/expand.h"
+#include "zsimd/attrib/flatten.h"
+#include "zsimd/attrib/target_clones.h"
+#include "zsimd/attrib/target.h"
+#include "zsimd/attrib/align.h"
