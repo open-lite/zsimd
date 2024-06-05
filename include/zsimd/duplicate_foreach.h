3fdf19734434b938490df2739158b5c1a4e6c062
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2024-06-05 17:45:30 -0600

    Remove not-yet-implemented architectures from duplicate_foreach.h

--- include/zsimd/duplicate_foreach.h
+++ include/zsimd/duplicate_foreach.h
@@ -1,5 +1,5 @@
-#ifndef __ZSIMD_DUPLICATE
-#define __ZSIMD_DUPLICATE
+#ifndef ZSIMD_DUPLICATE_FOREACH
+#define ZSIMD_DUPLICATE_FOREACH
 #include "zsimd/attrib/current_arch.h"
 
 
@@ -10,80 +10,80 @@
 
 /** x86 **/
 #ifdef ZSIMD_X86
-    //SSE
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH sse
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
+    ////SSE
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH sse
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
 
-    //SSE2
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH sse2
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
+    ////SSE2
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH sse2
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
 
-    //SSE3
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH sse3
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
+    ////SSE3
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH sse3
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
 
-    //SSSE3
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH ssse3
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
+    ////SSSE3
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH ssse3
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
 
-    //SSE4
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH sse4
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
+    ////SSE4
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH sse4
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
 
-    //SSE4a
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH sse4a
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
-
-
-    #undef ZSIMD_ARCH_TARGET
-
-    //SSE4.1
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH sse4_1
-    #define ZSIMD_ARCH_TARGET sse4.1
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
-
-    //SSE4.2
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH sse4_2
-    #define ZSIMD_ARCH_TARGET sse4.2
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
-
-    #define ZSIMD_ARCH_TARGET ZSIMD_ARCH
+    ////SSE4a
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH sse4a
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
 
 
-    //AVX
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH avx
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
+    //#undef ZSIMD_ARCH_TARGET
 
-    //AVX2
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH avx2
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
+    ////SSE4.1
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH sse4_1
+    //#define ZSIMD_ARCH_TARGET sse4.1
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
+
+    ////SSE4.2
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH sse4_2
+    //#define ZSIMD_ARCH_TARGET sse4.2
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
+
+    //#define ZSIMD_ARCH_TARGET ZSIMD_ARCH
 
 
-    //AVX512f
-    #undef ZSIMD_ARCH
-    #define ZSIMD_ARCH avx512f
-    #include __ZSIMD_EXPAND_FILE
-    #include ZSIMD_SELF_INCLUDE_PATH
+    ////AVX
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH avx
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
+
+    ////AVX2
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH avx2
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
+
+
+    ////AVX512f
+    //#undef ZSIMD_ARCH
+    //#define ZSIMD_ARCH avx512f
+    //#include __ZSIMD_EXPAND_FILE
+    //#include ZSIMD_SELF_INCLUDE_PATH
 
 /** ARM Neon/SVE **/
 #elif ZSIMD_ARM
@@ -100,5 +100,5 @@
 #include __ZSIMD_EXPAND_FILE
 
 
-#undef __ZSIMD_DUPLICATE
+#undef ZSIMD_DUPLICATE_FOREACH
 #endif
